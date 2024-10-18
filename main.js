dabf5f187894ed8bc47af1e72cfb218cc16c95f4
Caillouu <lannier.pierre@gmail.com>
2024-10-18 16:18:44 +0200

    structure page

--- main.js
+++ main.js
@@ -9,8 +9,14 @@ import './src/assets/scss/_footer.scss'
 import './src/assets/scss/components/_slider.scss'
 import './src/assets/scss/components/_bk-news.scss'
 import './src/assets/scss/components/_bk-card.scss'
+import './src/assets/scss/components/_bk-timeline.scss'
+
+import $ from 'jquery';
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
+
+window.$ = $;
 
 const swiper = new Swiper(".swiper-home", {
   loop: true,
@@ -18,6 +24,7 @@ const swiper = new Swiper(".swiper-home", {
     delay: 2500,
     disableOnInteraction: false,
   },
+  modules: [Navigation, Pagination],
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -32,13 +39,49 @@ const swiper = new Swiper(".swiper-home", {
 });
 
 
-const bkCards = document.querySelectorAll('.bk-card');
 
-bkCards.forEach(card => {
-  const color = card.getAttribute('data-color');
-  console.log(card.querySelector('.bk-card__title'))
-  card.querySelector('.bk-card__title').style.color = color;
-  card.querySelector('.bk-card__img').style.backgroundColor = color;
-  card.querySelector('.bk-card__bg').style.backgroundColor = color;
-  card.querySelector('.btn').style.backgroundColor = color;
-});
+$('.bk-card').each(function() {
+  const color = $(this).data('color')
+  $(this).find('.bk-card__title').css('color', color)
+  $(this).find('.bk-card__img').css('background-color', color)
+  $(this).find('.bk-card__bg').css('background-color', color)
+  $(this).find('.btn').css('background-color', color)
+})
+
+$('.bk-card').on('mouseenter', function(e) {
+  const color = $(this).data('color')
+  $(this).find('.btn').css('color', color)
+}).on('mouseleave', function(e) {
+  $(this).find('.btn').css('color', '#fff')
+})
+
+
+$('.bk-timeline__icon').on('click', function() {
+  const parent = $(this).closest('.bk-timeline')
+
+  if(parent.hasClass('bk-timeline--open')) {
+    parent.find('.bk-timeline__content').slideUp(500,  function() {
+      parent.removeClass('bk-timeline--open')
+    })
+  } else {
+    parent.addClass('bk-timeline--open')
+    parent.find('.bk-timeline__content').slideDown()
+  }
+})
+
+$('.bk-news').each(function() {
+  const $paragraph = $(this).find('.bk-news__content p')
+  const text = $paragraph.text()
+  $paragraph.attr('data-text', text)
+  $paragraph.text(text.trim().substring(0, 110) + "...")
+})
+
+$('.bk-news').on('mouseenter', function() {
+  const $paragraph = $(this).find('.bk-news__content p')
+  const fullText = $paragraph.attr('data-text')
+  $paragraph.text(fullText) 
+}).on('mouseleave', function() {
+    const $paragraph = $(this).find('.bk-news__content p')
+    const text = $paragraph.text()
+    $paragraph.text(text.trim().substring(0, 110) + "...")
+  })
